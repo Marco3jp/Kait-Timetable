135e307cd10204ac62f65cb3520015e278f7c607
Marco3jp <28888207+Marco3jp@users.noreply.github.com>
2018-05-06 18:40:59 +0900

    idの解釈を間違えていたので修正した

--- js/view/displayLectureInfo.js
+++ js/view/displayLectureInfo.js
@@ -7,7 +7,6 @@ class DisplayLectureInfo {
 
     displayQuickMode(id) {
         dbController.getLectureInfo(id).then(info =>{
-            console.log(info);
             if(info.noData){
                 this.name.textContent = "授業なし";
                 this.classRoom.textContent = "";
@@ -16,14 +15,27 @@ class DisplayLectureInfo {
                 this.classRoom.textContent = infoErrHandler.isUndefined(info.classRoom) ? "" : classRoomParser.parse(info.classRoom);
                 // undefined時、空文字列の挿入
             }
+            
+            let periodText = "";
 
             switch (id % 10){
-                case 0 : this.period.textContent = "一限目(9:30〜11:00)" ; break;
-                case 1 : this.period.textContent = "二限目(11:10〜12:40)" ; break;
-                case 2 : this.period.textContent = "三限目(13:20〜14:50)" ; break;
-                case 3 : this.period.textContent = "四限目(15:00〜16:30)" ; break;
-                case 4 : this.period.textContent = "五限目(16:40〜18:10)" ; break;
+                case 0 : periodText = "月 : " ; break;
+                case 1 : periodText = "火 : " ; break;
+                case 2 : periodText = "水 : " ; break;
+                case 3 : periodText = "木 : " ; break;
+                case 4 : periodText = "金 : " ; break;
+                case 5 : periodText = "土 : " ; break;
             }
+
+            switch ( Math.floor(id / 10) ){
+                case 0 : periodText += "一限目(9:30〜11:00)" ; break;
+                case 1 : periodText += "二限目(11:10〜12:40)" ; break;
+                case 2 : periodText += "三限目(13:20〜14:50)" ; break;
+                case 3 : periodText += "四限目(15:00〜16:30)" ; break;
+                case 4 : periodText += "五限目(16:40〜18:10)" ; break;
+            }
+
+            this.period.textContent = periodText;
         });
     }
 
@@ -37,7 +49,6 @@ class DisplayLectureInfo {
 
     displayEditModeAllCell(){
         dbController.getAllLectureInfo().then(info => {
-            console.log(info);
             for (let i = 0 ; i < info.length ; i++) {
                 this.displayCell({id:info[i].id,displayName:info[i].displayName,classRoom:info[i].classRoom});
             }
@@ -45,9 +56,7 @@ class DisplayLectureInfo {
     }
 
     displayCell(oneLecture){
-        console.log(oneLecture);
         const element = document.querySelector("th[data-id=\""+ oneLecture.id.toString() +"\"]");
-        console.log(element);
         element.innerHTML = (infoErrHandler.isUndefined(oneLecture.displayName) ? "a" : oneLecture.displayName) +"<br><hr>" + (infoErrHandler.isUndefined(oneLecture.classRoom) ? "a" : classRoomParser.parse(oneLecture.classRoom) );
     }
 }
