41cb6311cd00d58e2ed914d2e36416244b2cd39f
Marco3jp <28888207+Marco3jp@users.noreply.github.com>
2018-05-06 18:36:27 +0900

    一限のときに前の講義を参照するときなど、コーナーケースの処理を追加

--- js/utils/periodParser.js
+++ js/utils/periodParser.js
@@ -10,13 +10,24 @@ class PeriodParser {
         return this.idCheck(id);
     }
 
-    idCheck(id){
-        if( (!saturdayFlag && id === 50) || (saturdayFlag && id === 60) ){
+    idCheck(id){ // 縦 +- 10 , 横 +- 1
+        if( (!saturdayFlag && (id % 10) >= 5) || (saturdayFlag && (id % 10) >= 6) ) { // 土曜日の有無に合わせて、idがそれ以上の値になっている場合
             return 0;
-        }else if( (!fivePeriodFlag && id % 10 > 3) || (fivePeriodFlag && id % 10 > 4)){
-            let value = Math.floor(id/10)*10+10;
-            return this.idCheck(value);
-        }else{
+        }else if(id === -10) { // 月曜日一限(id 0)のとき、スイッチを使って前の講義を参照した場合
+            return saturdayFlag
+                ? fivePeriodFlag
+                    ? 45
+                    : 35
+                : fivePeriodFlag
+                    ? 44
+                    : 34;
+        }else if(id < 0){ // 月曜日以外の一限のとき、スイッチを使って前の講義を参照した場合
+            return fivePeriodFlag
+                ? (id+49)
+                : (id+39);
+        }else if( (!fivePeriodFlag && Math.floor(id / 10) > 3) || (fivePeriodFlag && Math.floor(id / 10) > 4)){ //四限と五限以上の値になっている場合
+            return this.idCheck(id%10+1);
+        }else{ // 通常時
             return id;
         }
     }
